ea4f01050aaccee7edb7ca38052ad4bb5a5d38d6
awadyaljabri <ziphone5sb@gmail.com>
2019-02-16 03:28:41 +0300

    Initial Commit

---
+++ StoreApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoreApp
+//
+//  Created by Awady | عوادي on 11/06/1440 AH.
+//  Copyright © 1440 Awady | عوادي. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
